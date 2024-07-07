1815abd6f7ad010113160f8687d1508b28348c90
marty1885 <marty188586@gmail.com>
2024-07-07 06:09:23 +0000

    fix conversion

--- convert.py
+++ convert.py
@@ -2,6 +2,6 @@ from rknn.api import RKNN
 
 rknn = RKNN()
 rknn.config(target_platform='rk3588')
-rknn.load_onnx("super-resolution-10.onnx")
+rknn.load_onnx("super-resolution-10.onnx", inputs=['input'], input_size_list=[[1, 1, 224, 224]])
 rknn.build(do_quantization=False)
 rknn.export_rknn("super-resolution-10.rknn")
